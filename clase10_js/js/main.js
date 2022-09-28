a8642ae5a0be568a5a352ccd5d4cfb0e4d284df1
Marcosotola <marcosotola@gmail.com>
2022-09-27 22:18:42 -0300

    intro js

---
+++ clase10_js/js/main.js
@@ -0,0 +1,16 @@
+
+console.log("hola mundo desde archivo externo");
+//Variables
+let nombre= "Marco";
+let apellido= "Sotola";
+let edad= 42;
+let mascota= true;
+
+console.log(nombre)
+console.log(apellido)
+console.log(edad)
+console.log(mascota)
+
+//operaciones
+
+alert("marco"+" "+"sotola" +(9+10))
